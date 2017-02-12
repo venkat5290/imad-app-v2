6c15ac6a41eb1eee19d0b8aa0f6c09d49fcae369
venkat5290 <injarapu.venkat@gmail.com>
2017-02-13 01:24:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/counter',function(req,res)
 {
     counter=counter+1;
     res.send(counter.toString());
-})
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
