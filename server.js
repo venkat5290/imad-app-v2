63ae12b279c9ed8e3c89d87e25f2903f8703ee79
venkat5290 <injarapu.venkat@gmail.com>
2017-02-13 01:25:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
 
 app.get('/:pagename',function(req,res){
     
@@ -98,12 +105,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-var counter=0;
-app.get('/counter',function(req,res)
-{
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
