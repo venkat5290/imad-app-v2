f7f8b20ab72123b678e62bf5689728b29062bb6a
venkat5290 <injarapu.venkat@gmail.com>
2017-02-13 00:55:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Executed Perfectly..Congratulations!!!!!');
 var e=document.getElementById('main-body');
 e.innerHTML="new value";
+
+var img=document.getElementById('im');
+img.onclick=myfunction ()
+{
+  img.style.marginleft="100px";  
+}
