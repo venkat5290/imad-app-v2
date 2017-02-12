7204dd16888759830c4895f65bf12304413a9edc
venkat5290 <injarapu.venkat@gmail.com>
2017-02-12 22:27:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 app.get('/page-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
 });
-app.get('/page-two', function(req,res) {
-  res.send("This is page two which is modified");
+app.get('/page-two',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'page-two.html'));
 });
-app.get('/page-three', function(req,res) {
-  res.send("This is page three which is modified");
+app.get('/page-three',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'page-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
