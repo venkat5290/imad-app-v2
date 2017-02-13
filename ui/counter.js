94adc010d7a26dd476d78c9208e671cb05ded6c4
venkat5290 <injarapu.venkat@gmail.com>
2017-02-14 00:31:35 +0530

    [imad-console] Updates ui/counter.js

--- ui/counter.js
+++ ui/counter.js
@@ -1,1 +1,8 @@
-
+var counetr=0;
+var button=document.getElementById("counter");
+button.onclick=functio()
+{
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter;
+}
