97486989bd5c7d0565ba5b564fa978e318630280
venkat5290 <injarapu.venkat@gmail.com>
2017-02-14 14:03:36 +0530

    [imad-console] Updates ui/counter.js

--- ui/counter.js
+++ ui/counter.js
@@ -29,13 +29,13 @@ var txtbox=input.value;
 var submit=document.getElementById("submit_b");
 submit.onclick=function()
 {
-var names=['name1','name2','name3'];
-var list='';
-for(var i=0;i<names.length;i++)
-{
-    list+='<li>'+names[i]+'</li>';
-}
-var lis=document.getElementById("list");
-lis.innerHTML=list;
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var lis=document.getElementById("name_list");
+    lis.innerHTML=list;
 };
 
