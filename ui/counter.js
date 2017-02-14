a327c249ba595c884c964c6958da78f2846ebecb
venkat5290 <injarapu.venkat@gmail.com>
2017-02-14 13:52:59 +0530

    [imad-console] Updates ui/counter.js

--- ui/counter.js
+++ ui/counter.js
@@ -31,7 +31,7 @@ submit.onclick=function()
 {
 var names=['name1','name2','name3'];
 var list='';
-for(var i=0;i<=names.length;i++)
+for(var i=0;i<names.length;i++)
 {
     list+='<li>'+names[i]+'</li>';
 }
