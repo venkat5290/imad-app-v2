871bb9426fa93471ca49a896576d683056a9696e
venkat5290 <injarapu.venkat@gmail.com>
2017-02-14 13:52:52 +0530

    [imad-console] Updates ui/counter.js

--- ui/counter.js
+++ ui/counter.js
@@ -23,3 +23,18 @@ button.onclick=function()
     reqob.open('GET','http://venkat5290.imad.hasura-app.io/counter','true');
     reqob.send(null);
 };
+
+var input=document.getElementById("input_box");
+var txtbox=input.value;
+var submit=document.getElemetById("submit_b");
+submit.onclick=function()
+{
+var names=['name1','name2','name3'];
+var list='';
+for(var i=0;i<=names.length;i++)
+{
+    list+='<li>'+names[i]+'</li>';
+}
+submit.innerHtml=list;
+};
+
