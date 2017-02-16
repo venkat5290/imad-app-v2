ddd2bfc41f64fad24267667d0cb5b60db2db62ac
venkat5290 <injarapu.venkat@gmail.com>
2017-02-16 14:38:23 +0530

    [imad-console] Updates ui/artcom.js

--- ui/artcom.js
+++ ui/artcom.js
@@ -1,1 +1,27 @@
+var submit=document.getElementById("submit-btn");
+submit.onclick=function()
+{
+    var req=new XMLHttpRequest();
+    req.onreadystatechange=function()
+    {
+         if(req.readyState===req.XMLHttpRequest.DONE)
+         {
+             if(req.status===200)
+             {
+                 comment=JSON.parse(this.responseText);
+                  var list='';
+               for(var i=0;i<comment.length;i++)
+               {
+                   list+='<li>'+comment[i]+'</li>';
+               }
+               var lis=document.getElementById("comment");
+               lis.innerHTML=list;
+             }
+         }
+    }
+    var input=document.getElementById("comment-box");
+    var comment=input.value;
+    reqob.open('GET','http://venkat5290.imad.hasura-app.io/add-comment/:comment','true');
+    reqob.send(null);
+}
 
