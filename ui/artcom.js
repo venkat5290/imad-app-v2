3c4dc47fa484fc1dc3f19c6bab3e067cd5080572
venkat5290 <injarapu.venkat@gmail.com>
2017-02-16 14:40:30 +0530

    [imad-console] Updates ui/artcom.js

--- ui/artcom.js
+++ ui/artcom.js
@@ -1,27 +1,76 @@
-var submit=document.getElementById("submit-btn");
+/*var button=document.getElementById("counter");
+button.onclick=function()
+{
+    //creating reuest object
+    var reqob=new XMLHttpRequest();
+    
+    //response code
+    reqob.onreadystatechange=function()
+    {
+        if(reqob.readyState===XMLHttpRequest.DONE)
+        {
+            if(reqob.status===200)
+            {
+                var counter=reqob.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        
+    }
+    
+    reqob.open('GET','http://venkat5290.imad.hasura-app.io/counter','true');
+    reqob.send(null);
+};
+
+*/
+
+//program to take name value from text field and send it to server and server returns us response that is typed name by using end point 
+
+
+var submit=document.getElementById("submit_b");
 submit.onclick=function()
 {
-    var req=new XMLHttpRequest();
-    req.onreadystatechange=function()
+    
+     //creating reuest object
+    var reqob=new XMLHttpRequest();
+    
+    //response code
+    reqob.onreadystatechange=function()
     {
-         if(req.readyState===req.XMLHttpRequest.DONE)
-         {
-             if(req.status===200)
-             {
-                 comment=JSON.parse(this.responseText);
-                  var list='';
-               for(var i=0;i<comment.length;i++)
+        if(reqob.readyState===XMLHttpRequest.DONE)
+        {
+            if(reqob.status===200)
+            {
+               //var response_string=reqob.reponseText;
+               names=JSON.parse(this.responseText);
+               var list='';
+               for(var i=0;i<names.length;i++)
                {
-                   list+='<li>'+comment[i]+'</li>';
+                   list+='<li>'+names[i]+'</li>';
                }
-               var lis=document.getElementById("comment");
+               var lis=document.getElementById("name_list");
                lis.innerHTML=list;
-             }
-         }
+            }
+        }
+        
     }
-    var input=document.getElementById("comment-box");
-    var comment=input.value;
-    reqob.open('GET','http://venkat5290.imad.hasura-app.io/add-comment/:comment','true');
+    var input=document.getElementById("input_box");
+    var txtbox=input.value;
+    reqob.open('GET','http://venkat5290.imad.hasura-app.io/submit-name?name='+txtbox,'true');
     reqob.send(null);
-}
+    
+    
+    
+    
+   /* var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var lis=document.getElementById("name_list");
+    lis.innerHTML=list; */
+};
 
