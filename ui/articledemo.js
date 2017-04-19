bc87fe057f561faf67992b9113d81fed9b0206eb
venkat5290 <injarapu.venkat@gmail.com>
2017-04-19 22:42:10 +0530

    Create articledemo.js

---
+++ ui/articledemo.js
@@ -0,0 +1,34 @@
+/*var articles={
+  'page-one': {
+     title : 'Article-one | venky',
+    heading : 'Article-one',
+    date : '2nd feb 2016',
+    content : `<p>this is my first article page.this is my first article page.this is my first article pagethis is my first article page
+            this is my first article pagethis is my first article pagethis is my first article pagethis is my first article page.this is my first article page.this is my first article pagethis is my first article page
+            this is my first article pagethis is my first article pagethis is my first article page
+            </p>
+            <p>this is my first article page.this is my first article page.this is my first article pagethis is my first article page
+            this is my first article pagethis is my first article pagethis is my first article pagethis is my first article page.this is
+            first article page.this is my first article pagethis is my first article page this is my first article pagethis is my firs
+            article pagethis is my first article page
+            </p>
+            <p>this is my first article page.this is my first article page.this is my first article pagethis is my first article page
+            this is my first article pagethis is my first article pagethis is my first article pagethis is my first article page.this is my
+            first article page.this is my first article pagethis is my first article page this is my first article pagethis is my fir
+            article pagethis is my first article page
+            </p> `
+},
+ 'page-two':{
+      title : 'Article-one | venky',
+    heading : 'Article-two',
+    date : '2nd feb 2016',
+    content : '<p>this is second content page</p>'
+ },
+ 'page-three':{
+     title : 'Article-three | venky',
+    heading : 'Article-three',
+    date : '2nd feb 2016',
+    content : '<p>this is third content page</p>'
+}
+
+};  */
